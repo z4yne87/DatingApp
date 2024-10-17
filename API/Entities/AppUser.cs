6377d80fc0d2ba5ce5c7d6b612e7e48dd2c91943
Oliver Boustead <olly.boustead@gmail.com>
2024-10-17 16:42:56 +0100

    End of Section 14

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -17,4 +17,6 @@ public class AppUser
     public required string City { get; set; }
     public required string Country { get; set; }
     public List<Photo> Photos { get; set; } = [];
+    public List<UserLike> LikedByUsers { get; set; } = [];
+    public List<UserLike> LikedUsers { get; set; } = [];
 }
